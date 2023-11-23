88c1f8dff4d65a5b3294eaea45b2c2253fe45f44
michal <michal.radek.nowakowski@gmail.com>
2023-11-23 17:55:41 +0100

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,7 +4,14 @@ import ModalContent from './ModalContent';
 export const Content = () => {
 
   const [isModalOpen, setOpenModal] = useState(false);
-  
+  const [taskList, setTaskList] = useState([]);
+
+  const printList = () => {
+    taskList.push("test");
+    console.log(taskList);
+  }
+
+  printList();
   const openModal = () => {
     setOpenModal(true);
   }
@@ -13,16 +20,18 @@ export const Content = () => {
     setOpenModal(false);
   }
 
+  const handleSave = (tekst) => {
+    console.log(tekst);
+  }
   return (
     <div className='contentWrapper'>
       <div className='buttonDiv'>
         <button className='button addButton' onClick={openModal}>Add task</button>
-        <ModalContent isOpen={isModalOpen} closeModal={closeModal}/>
+        <ModalContent isOpen={isModalOpen} closeModal={closeModal} onSave={handleSave}/>
         <button className='button deleteAllButton'>Delete tasks</button>
         <button className='button deleteDoneButton'>Delete done tasks</button>
       </div>
       <div className="contentDiv">
-        <h1>ToDoList</h1>
       </div>
     </div>
   )
--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 const CustomStyles = {
-
   content: {
-    backgroundColor: 'grey',
-    border: 'none',
+    backgroundColor: "grey",
+    border: "none",
+    height: "15vh",
   },
   overlay: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
+  },
+};
+
+export const ModalContent = ({ isOpen, closeModal, onSave}) => {
+
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
   }
-}
-export const ModalContent = ({isOpen, closeModal}) => {
-  return (  
+
+  const handleSaveButtonClick = () => {
+    
+    if(inputValue.trim() === "") {
+      alert("Write your task");
+    }
+    else {
+      onSave(inputValue);
+    }
+  }
+
+  return (
     <Modal isOpen={isOpen} onRequestClose={closeModal} style={CustomStyles}>
-      <div className='modalDivContent'> 
-        <input placeholder='Write your task here' type='text' className="modalInput"></input>
-        <div className='modalButtonDiv'>
-          <button className='modalButton'>Save</button>
-          <button onClick={closeModal} className='modalButton'>Close</button>
+      <div className="modalDivContent">
+        <input
+          placeholder="Write your task here"
+          type="text"
+          className="modalInput"
+          onChange={(e) => handleInputChange(e.target.value)}></input>
+        <div className="modalButtonDiv">
+          <button className="modalButton modalSaveButton" onClick={handleSaveButtonClick}>Save</button>
+          <button onClick={closeModal} className="modalButton modalCloseButton">Close</button>
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalContent
+export default ModalContent;
