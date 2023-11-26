678cd2d1743f03b45d3cfeb764d09f3cf6751d30
michalradek <michal.radek.nowakowski@gmail.com>
2023-11-26 20:59:18 +0100

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -15,28 +15,39 @@ export const Content = () => {
   }
 
   const handleSave = (tekst) => {
-    setTaskList([...taskList, tekst]);
-    
+    setTaskList([...taskList, {text: tekst, done: false}]);
   }
 
   const deleteAllTasks = () => {
     setTaskList([]);
   }
 
+  const deleteDoneTasks = () => {
+    const newTaskList = taskList.filter(task => !task.done);
+    setTaskList(newTaskList);
+ }
+
+ const toggleTaskDone = (index) => {
+    const newTaskList = [...taskList];
+    newTaskList[index].done = !newTaskList[index].done;
+    setTaskList(newTaskList);
+ }
+
   return (
     <div className='contentWrapper'>
       <div className='buttonDiv'>
         <button className='button addButton' onClick={openModal}>Add task</button>
         <ModalContent isOpen={isModalOpen} closeModal={closeModal} onSave={handleSave}/>
-        <button className='button deleteAllButton' onClick={deleteAllTasks}>Delete tasks</button>
-        <button className='button deleteDoneButton'>Delete done tasks</button>
+        <button className='button deleteAllButton'>Delete tasks</button>
+        <button className='button deleteDoneButton' onClick={deleteDoneTasks}>Delete done tasks</button>
       </div>
       <div className="contentDiv">
         {taskList.map((element, index) => {
           return (
             <div key={index} className='listDiv'>
-              <input type="checkbox" id="contentDivInput"></input>
-              <label For="contentDivInput" className='inputLabel'>{element}</label>
+              <input type="checkbox" id="contentDivInput" className="contentDivInput"
+              checked={element.done} onChange={() => toggleTaskDone(index)}></input>
+              <label For="contentDivInput" className='inputLabel'>{element.text}</label>
             </div>
           )
         })}
