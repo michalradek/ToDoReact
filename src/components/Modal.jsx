b20b72f7f0e647a72953fef8428f03a0be950a30
michalradek <michal.radek.nowakowski@gmail.com>
2023-12-01 21:41:49 +0100

    commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Modal = ({ open, onClose }) => {
+export const Modal = ({ open, onClose, onSave }) => {
 
+    const [inputValue, setInputValue] = useState("");
     if(!open) return null
 
+    const handleChangeInput = event => {
+        setInputValue(event.target.value);
+    }
+
+    const handleClose = () => {
+        onClose();
+        setInputValue("");
+    }
+
+    const handleSave = () => {
+        if(inputValue.trim() === "") {
+            alert("Please write your task");
+        }
+        else {
+            onSave(inputValue);
+            setInputValue("");
+        }
+    }
     return (
         <div className="overlay">
             <div className="modalContainer">
-            <p className="modalCloseButtonX" onClick={onClose}>x</p>
-                    <input className="modalInput" type="text" placeholder="Write your task here"></input>
+            <p className="modalCloseButtonX" onClick={handleClose}>X</p>
+                    <input className="modalInput" type="text" placeholder="Write your task here" onChange={handleChangeInput} value={inputValue}></input>
                     <div className="modalButtonDiv">
-                        <button className="modalSaveButton modalButton">Save</button>
-                        <button className="modalCloseButton modalButton" onClick={onClose}>Close</button>
+                        <button className="modalSaveButton modalButton" onClick={handleSave}>Save</button>
+                        <button className="modalCloseButton modalButton" onClick={handleClose}>Close</button>
                     </div>
             </div>
         </div>
